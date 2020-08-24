c15360f778aa6d9545bbf60b47264a6f6cefc6c9
k3nnywilliam <k3nnywilliam@gmail.com>
2020-08-24 23:33:18 +0800

    Fix time logging

--- build/genoken/modules_test.py
+++ build/genoken/modules_test.py
@@ -1,23 +1,22 @@
 import unittest
 from fasta import fastautil as fst
-from miscutil.util import *
+from miscutil.util import Utils as util
 
 class GenoKenTest(unittest.TestCase):
     
-    @timer_logging
+    @util.timer_logging
     def test_read_dna_file(self):
         pass
     
     def test_transcribe(self):
         pass
     
-    @timer_logging
+    @util.timer_logging
     def test_blast_record(self):
         pass
 
     def test_multifasta(self):
-        util = fst.FastaUtils("/data/dna.example.fa")
-        util.get_multifasta_record()
+        pass
 
     def test_reverse_complement(self):
         pass
