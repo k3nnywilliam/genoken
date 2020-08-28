5862be43d0685ea6628809c2ee9c03e251076706
Kenny Nyallau <k3nnywilliam@gmail.com>
2020-08-28 14:19:25 +0800

    Fix fastautil import

--- build/genoken/fasta_util_test.py
+++ build/genoken/fasta_util_test.py
@@ -1,11 +1,11 @@
-from fasta import fastautil as fst
+from fasta.fastautil import FastaUtils
 from miscutil.util import GenokenUtils
 from Bio import SeqIO
 import os
 
 def main():
     DATA_DIR = os.path.join(os.path.abspath(os.curdir), 'data')
-    genotest = fst.FastaUtils(os.path.join(DATA_DIR, 'dna2.fasta'))
+    genotest = FastaUtils(os.path.join(DATA_DIR, 'dna2.fasta'))
     #records_len, record_seqs, record_ids, record_counts, \
     dict_id_seqs  = genotest.get_fasta_record_info()
     #genotest.start_ORF(frame=2)
