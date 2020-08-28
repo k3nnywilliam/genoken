a2c5e20fcc7af6f61059f842f28e3cb6a185eacb
Kenny Nyallau <k3nnywilliam@gmail.com>
2020-08-28 17:59:11 +0800

    Update modules tests

--- build/genoken/modules_test.py
+++ build/genoken/modules_test.py
@@ -1,25 +1,28 @@
 import unittest
-from fasta import fastautil as fst
 from miscutil.util import GenokenUtils
+from dnautil.dnautil import DNAUtils
+
+dna = 'ATGATAGGCCTA'
+du = DNAUtils(dna)
 
 class GenoKenTest(unittest.TestCase):
-    
-    @GenokenUtils.timerlogging
+
     def test_read_dna_file(self):
         pass
     
     def test_transcribe(self):
         pass
     
-    @GenokenUtils.timerlogging
     def test_blast_record(self):
         pass
-
-    def test_multifasta(self):
-        pass
-
+    
+    @GenokenUtils.timerlogging
+    def test_complement(self):
+        assert (du.complement()) == 'TACTATCCGGAT'
+    
+    @GenokenUtils.timerlogging
     def test_reverse_complement(self):
-        pass
+        assert (du.reverse_complement()) == 'TAGGCCTATCAT'
 
 if __name__ == '__main__':
     runner = unittest.TextTestRunner(verbosity=2)
