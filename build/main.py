5b5c7ae6e4f476f64841eb8dc8f92609a691dd34
Kenny Nyallau <k3nnywilliam@gmail.com>
2021-03-12 19:09:22 +0800

    remove some unnecessary files

--- build/main.py
+++
@@ -1,8 +0,0 @@
-import sys
-from PyQt5 import QtWidgets as QtW
-from view import MainWindow
-
-if __name__ == '__main__':
-    app = QtW.QApplication(sys.argv)
-    window = MainWindow()
-    sys.exit(app.exec_())
